33d19d2afd29ead33b1701ae5e39aa905b7ca273
edyespinal <contacto@edyespinal.com>
2021-05-10 18:30:07 +0200

    feat: add order tracking

--- react/index.tsx
+++ react/index.tsx
@@ -4,7 +4,32 @@ import type { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView': {
+    case 'vtex:orderPlaced': {
+      const sc = document.createElement('span')
+      const {
+        ordersInOrderGroup: [orderId],
+        visitorContactInfo: [customerEmail],
+        transactionProducts,
+      } = e.data
+
+      const products = transactionProducts.map(product => {
+        const { id, quantity, price } = product
+
+        return {
+          id,
+          quantity,
+          price,
+        }
+      })
+
+      sc.classList.add('clerk')
+      sc.setAttribute('data-api', 'log/sale')
+      sc.setAttribute('data-sale', orderId)
+      sc.setAttribute('data-email', customerEmail)
+      sc.setAttribute('data-products', JSON.stringify(products))
+
+      document.body.appendChild(sc)
+
       break
     }
 
