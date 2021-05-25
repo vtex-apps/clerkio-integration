5482b240c343a87eec7bff4679fcc322d36b2c8c
Filadelfo <filadelfo.braz@gmail.com>
2021-05-25 18:43:12 +0200

    Feat: Call Clerk content manually after React renders the component

--- react/Block/index.tsx
+++ react/Block/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ClerkIoBlock = () => {
+  useEffect(() => {
+    const { Clerk } = window
+
+    Clerk('content', '.clerk-store-block')
+  }, [])
+
   return (
     <div>
       <h2>Clerk Io</h2>
-      <span className="clerk" data-template="@first-component-content" />
+      <span
+        className="clerk-store-block"
+        data-template="@first-component-content"
+      />
     </div>
   )
 }
